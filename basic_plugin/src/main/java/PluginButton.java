4d19dfb6d96df478c6d4653feba1a722014bde18
Valerii3 <vallerus55@gmail.com>
2023-03-27 23:56:57 +0200

    web view

--- basic_plugin/src/main/java/PluginButton.java
+++ basic_plugin/src/main/java/PluginButton.java
@@ -1,22 +1,24 @@
+import com.intellij.ide.BrowserUtil;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.ui.Messages;
 import org.jetbrains.annotations.NotNull;
 
+
+
+import javax.swing.*;
+
+
 public class PluginButton extends AnAction {
-
-
   @Override
   public void actionPerformed(@NotNull AnActionEvent e) {
-    Messages.showMessageDialog("Hello", "Hello World", Messages.getInformationIcon());
+    String[] options = {"WebTeam people", "JCEF"};
+    int choice = JOptionPane.showOptionDialog(null, "Choose a website to open", "Website Selector",
+        JOptionPane.YES_NO_OPTION, JOptionPane.PLAIN_MESSAGE, null, options, options[0]);
+
+    if (choice == 0) {
+      BrowserUtil.browse("https://www.jetbrains.com/company/people/webteam/");
+    } else if (choice == 1) {
+      BrowserUtil.browse("https://github.com/chromiumembedded/java-cef");
+    }
   }
-
-  @Override
-  public boolean isDumbAware() {
-    return super.isDumbAware();
-  }
-
-
-
-
 }
