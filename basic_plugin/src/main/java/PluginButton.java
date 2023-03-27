e3cf7b5b5ae672bbc825e8791d87be4977edf86f
Valerii3 <vallerus55@gmail.com>
2023-03-28 00:05:15 +0200

    JCEF

--- basic_plugin/src/main/java/PluginButton.java
+++ basic_plugin/src/main/java/PluginButton.java
@@ -1,6 +1,7 @@
-import com.intellij.ide.BrowserUtil;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.ui.jcef.JBCefBrowser;
+import java.awt.BorderLayout;
 import org.jetbrains.annotations.NotNull;
 
 
@@ -11,14 +12,27 @@ import javax.swing.*;
 public class PluginButton extends AnAction {
   @Override
   public void actionPerformed(@NotNull AnActionEvent e) {
-    String[] options = {"WebTeam people", "JCEF"};
+    String[] options = {"WevTeam people", "JCEF repository"};
     int choice = JOptionPane.showOptionDialog(null, "Choose a website to open", "Website Selector",
         JOptionPane.YES_NO_OPTION, JOptionPane.PLAIN_MESSAGE, null, options, options[0]);
 
     if (choice == 0) {
-      BrowserUtil.browse("https://www.jetbrains.com/company/people/webteam/");
+      JBCefBrowser browser = new JBCefBrowser();
+      browser.loadURL("https://www.jetbrains.com/company/people/webteam/");
+      showInDialog(browser.getComponent());
     } else if (choice == 1) {
-      BrowserUtil.browse("https://github.com/chromiumembedded/java-cef");
+      JBCefBrowser browser = new JBCefBrowser();
+      browser.loadURL("https://github.com/chromiumembedded/java-cef");
+      showInDialog(browser.getComponent());
     }
+  }
+
+  private static void showInDialog(@NotNull JComponent component) {
+    JFrame frame = new JFrame();
+    frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+    frame.getContentPane().add(component, BorderLayout.CENTER);
+    frame.setSize(800, 600);
+    frame.setLocationRelativeTo(null);
+    frame.setVisible(true);
   }
 }
